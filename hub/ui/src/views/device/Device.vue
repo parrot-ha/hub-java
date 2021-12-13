11052076f7b4e41b8155cfd02ace55416ab60874
parrot-ha <parrotha@protonmail.com>
2021-12-12 18:41:09 -0600

    add filter on device page for device handlers based on tags for integration.

--- hub/ui/src/views/device/Device.vue
+++ hub/ui/src/views/device/Device.vue
@@ -4,6 +4,77 @@
       <v-row>
         <v-col :cols="12">
           <v-card>
+            <v-card-title>Information</v-card-title>
+            <v-card-text>
+              <v-simple-table>
+                <tbody>
+                  <tr>
+                    <td>Date Created</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Last Updated</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Data</td>
+                    <td>
+                      <ul>
+                        <li
+                          v-for="(value, name, i) in information.data"
+                          :key="i"
+                        >
+                          {{ name }}:
+                          <strong>{{ value }}</strong>
+                        </li>
+                      </ul>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Current States</td>
+                    <td>
+                      <ul>
+                        <li
+                          v-for="currentState in currentStates"
+                          :key="currentState.name"
+                        >
+                          {{ currentState.name }}:
+                          <strong>{{ currentState.stringValue }}</strong>
+                        </li>
+                      </ul>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Events</td>
+                    <td>
+                      <a :href="`/device/${device.id}/events`">List Events</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>In Use By</td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </v-simple-table>
+            </v-card-text>
+            <v-card-actions> </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col :cols="12">
+          <v-card>
+            <v-card-title>Commands</v-card-title>
+            <v-card-text>
+              <ul v-for="(command, i) in commands" :key="i">
+                <device-command
+                  :deviceId="device.id"
+                  :command="command"
+                ></device-command>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col :cols="12">
+          <v-card>
             <v-card-title>Settings</v-card-title>
             <v-card-text>
               <v-form>
@@ -17,7 +88,14 @@
                   v-model="device.deviceNetworkId"
                 ></v-text-field>
                 <v-select
-                  :items="deviceHandlers"
+                  :items="integrations"
+                  :item-text="item => item.label"
+                  item-value="id"
+                  label="Integration"
+                  v-model="device.integrationId"
+                ></v-select>
+                <v-select
+                  :items="filteredDevices"
                   :item-text="item => item.name + ' (' + item.namespace + ')'"
                   item-value="id"
                   label="Type"
@@ -28,13 +106,6 @@
                   @click="dhFiltering = !dhFiltering"
                   v-text="dhFiltering ? 'mdi-filter' : 'mdi-filter-off'"
                 ></v-icon>
-                <v-select
-                  :items="integrations"
-                  :item-text="item => item.label"
-                  item-value="id"
-                  label="Integration"
-                  v-model="device.integrationId"
-                ></v-select>
               </v-form>
             </v-card-text>
 
@@ -162,77 +233,6 @@
             </v-card-actions>
           </v-card>
         </v-col>
-        <v-col :cols="12">
-          <v-card>
-            <v-card-title>Information</v-card-title>
-            <v-card-text>
-              <v-simple-table>
-                <tbody>
-                  <tr>
-                    <td>Date Created</td>
-                    <td></td>
-                  </tr>
-                  <tr>
-                    <td>Last Updated</td>
-                    <td></td>
-                  </tr>
-                  <tr>
-                    <td>Data</td>
-                    <td>
-                      <ul>
-                        <li
-                          v-for="(value, name, i) in information.data"
-                          :key="i"
-                        >
-                          {{ name }}:
-                          <strong>{{ value }}</strong>
-                        </li>
-                      </ul>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Current States</td>
-                    <td>
-                      <ul>
-                        <li
-                          v-for="currentState in currentStates"
-                          :key="currentState.name"
-                        >
-                          {{ currentState.name }}:
-                          <strong>{{ currentState.stringValue }}</strong>
-                        </li>
-                      </ul>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Events</td>
-                    <td>
-                      <a :href="`/device/${device.id}/events`">List Events</a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>In Use By</td>
-                    <td></td>
-                  </tr>
-                </tbody>
-              </v-simple-table>
-            </v-card-text>
-            <v-card-actions> </v-card-actions>
-          </v-card>
-        </v-col>
-        <v-col :cols="12">
-          <v-card>
-            <v-card-title>Commands</v-card-title>
-            <v-card-text>
-              <ul v-for="(command, i) in commands" :key="i">
-                <device-command
-                  :deviceId="device.id"
-                  :command="command"
-                ></device-command>
-              </ul>
-            </v-card-text>
-          </v-card>
-        </v-col>
       </v-row>
     </v-layout>
   </v-container>
@@ -261,6 +261,27 @@ export default {
   components: {
     DeviceCommand,
     EnumInput
+  },
+  computed: {
+    filteredDevices: function() {
+      if (this.dhFiltering && this.device.integrationId != null) {
+        var integration = this.integrations.find(
+          i => i.id == this.device.integrationId
+        );
+        if (integration.tags != null) {
+          return this.deviceHandlers.filter(
+            dh =>
+              (dh.tags != null &&
+                integration.tags.some(t => dh.tags.indexOf(t) >= 0)) ||
+              this.device.deviceHandlerId == dh.id
+          );
+        } else {
+          return this.deviceHandlers;
+        }
+      } else {
+        return this.deviceHandlers;
+      }
+    }
   },
   methods: {
     updatePreferenceLayout: function() {
@@ -327,7 +348,9 @@ export default {
   mounted: function() {
     this.deviceId = this.$route.params.id;
 
-    fetch('/api/integrations?type=DEVICE&field=id&field=name&field=label&field=tags')
+    fetch(
+      '/api/integrations?type=DEVICE&field=id&field=name&field=label&field=tags'
+    )
       .then(response => response.json())
       .then(data => {
         if (typeof data !== 'undefined' && data != null) {
