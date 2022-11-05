3cf14f40dc60b54af45eb3f22d17a713c7f5484f
parrot-ha <parrotha@protonmail.com>
2022-11-05 15:23:32 -0500

    remove extra table.  Fixes #47

--- hub/ui/src/views/app/InstalledAutomationAppList.vue
+++ hub/ui/src/views/app/InstalledAutomationAppList.vue
@@ -33,42 +33,6 @@
                   >
                 </template>
               </v-data-table>
-
-              <v-simple-table>
-                <thead>
-                  <tr>
-                    <th scope="col" style="width:4%"></th>
-                    <th scope="col" style="width:48%">Label</th>
-                    <th scope="col" style="width:48%">Type</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr
-                    v-for="installedAutomationApp in installedAutomationApps"
-                    :key="installedAutomationApp.id"
-                  >
-                    <td>
-                      <router-link
-                        :to="{
-                          name: 'InstalledAutomationAppInfo',
-                          params: { id: installedAutomationApp.id }
-                        }"
-                        ><v-icon>mdi-alert-circle-outline</v-icon></router-link
-                      >
-                    </td>
-                    <td>
-                      <router-link
-                        :to="{
-                          name: 'InstalledAutomationAppConfig',
-                          params: { id: installedAutomationApp.id }
-                        }"
-                        >{{ installedAutomationApp.label }}</router-link
-                      >
-                    </td>
-                    <td>{{ installedAutomationApp.type }}</td>
-                  </tr>
-                </tbody>
-              </v-simple-table>
             </v-card-text>
             <v-card-actions> </v-card-actions>
           </v-card>
