310d401e0de0c8f1c7a0684dd4b6785c98aa9ab1
parrot-ha <parrotha@protonmail.com>
2023-01-09 17:17:55 -0600

    remove unused style

--- hub/ui2/src/App.vue
+++ hub/ui2/src/App.vue
@@ -22,18 +22,3 @@ import AppNavigation from "./components/AppNavigation.vue";
 import AppHeader from "./components/AppHeader.vue";
 </script>
 
-<style scoped>
-.bd-placeholder-img {
-  font-size: 1.125rem;
-  text-anchor: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  user-select: none;
-}
-
-@media (min-width: 768px) {
-  .bd-placeholder-img-lg {
-    font-size: 3.5rem;
-  }
-}
-</style>
